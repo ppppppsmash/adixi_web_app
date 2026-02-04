9911a2895bf326e77dc10c44f4f35589e8569c4e
kurosawa <pppppp.work@gmail.com>
2026-02-04 10:51:03 +0900

    fix: Fix Components

--- src/components/background/LetterGlitch.tsx
+++ src/components/background/LetterGlitch.tsx
@@ -215,7 +215,10 @@ const LetterGlitch = ({
   }, [glitchSpeed, smooth]);
 
   return (
-    <div className="relative w-full min-h-screen h-screen bg-black overflow-hidden">
+    <div
+        className="relative w-full min-h-screen h-screen bg-black overflow-hidden"
+        style={{ transform: 'translateZ(0)' }}
+      >
       <canvas ref={canvasRef} className="block w-full h-full" />
       {outerVignette && (
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none bg-[radial-gradient(circle,_rgba(0,0,0,0)_60%,_rgba(0,0,0,1)_100%)]"></div>
